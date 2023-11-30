861f37494f5813f8166124605d1ba819d3b32f6a
feewo <capi62@yandex.ru>
2023-11-30 20:28:28 +0300

    комменты к ошибке добавлены

--- handle.go
+++ handle.go
@@ -11,10 +11,6 @@ import (
 	"taskmaster/db"
 	"taskmaster/engine"
 	"taskmaster/entity"
-<<<<<<< Updated upstream
-	_ "taskmaster/entity"
-=======
->>>>>>> Stashed changes
 	"time"
 )
 
@@ -60,17 +56,13 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	url := r.URL
 	path := url.Path[1:]
 	pathArr := strings.Split(path, "/")
-	// fmt.Println(reflect.TypeOf(path))
-	// if pathArr[0] == "" {
-	// 	sendFile("front/index.html", ctx)
-	// 	return
-	// }
+	// вот тут начинаются проблемы
+	// не открывается HTML файл
 	if stat, ok := checkStatic(path); ok {
 		fmt.Println(stat, "ok")
 		sendFile(stat, ctx)
 		return
 	}
-
 	maps, ok := apiMap[r.Method]
 	if !ok {
 		w.Write([]byte("Нет метода"))
@@ -84,10 +76,6 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	if fun, ok := maps[pathName]; ok {
 		isIgnore := false
 		for _, s := range ignoreList {
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 			if s == pathName {
 				isIgnore = true
 				break
@@ -100,103 +88,44 @@ func handle(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			var tokenDb entity.Token
-<<<<<<< Updated upstream
 			db.DB().Table(tokenDb.TableName()).Where("token = ? and expired > ?", token, time.Now())
-=======
 			var userDb entity.User
 
 			db.DB().Table(tokenDb.TableName()).Where("token = ? and expired > ?", token, time.Now()).Find(&tokenDb)
 
->>>>>>> Stashed changes
 			if tokenDb.Iid == 0 {
 				ctx.Error(401, "Bad")
 				return
 			}
-<<<<<<< Updated upstream
-=======
 			db.DB().Table(userDb.TableName()).Where("iid = ?", tokenDb.Iid).Find(&userDb)
 			if (r.Method == "POST" || r.Method == "DELETE" || r.Method == "PUT") && userDb.Role == "user" {
 				ctx.Error(401, "Bad")
 				return
 			}
->>>>>>> Stashed changes
 		}
 		in := make([]reflect.Value, 1)
 		in[0] = reflect.ValueOf(&ctx)
 		fun.Call(in)
 		return
 	}
-
-	// POST
-	// if r.Method == "POST" {
-	// 	switch pathArr[0] {
-	// 	case "user":
-	// 		hdl.UserCreate(ctx)
-	// 	default:
-	// 		w.Write([]byte("Нет БО"))
-	// 	}
-	// 	return
-	// }
-	// // GET
-	// if r.Method == "GET" {
-	// 	switch pathArr[0] {
-	// 	case "user":
-	// 		if len(pathArr) > 1 {
-	// 			id := pathArr[1]
-	// 			hdl.User(&ctx, id)
-	// 		} else {
-	// 			hdl.Users(ctx)
-	// 		}
-	// 	default:
-	// 		w.Write([]byte("Нет БО"))
-	// 	}
-	// 	return
-	// }
-
-	// // DELETE
-	// if r.Method == "DELETE" {
-	// 	id := pathArr[1]
-	// 	switch pathArr[0] {
-	// 	case "user":
-	// 		if id != "" {
-	// 			hdl.UserDelete(ctx, id)
-	// 		} else {
-	// 			w.Write([]byte("Нет БО"))
-	// 		}
-	// 	default:
-	// 		w.Write([]byte("Нет БО"))
-	// 	}
-	// 	return
-	// }
-	// // PUT
-	// if r.Method == "PUT" {
-	// 	id := pathArr[1]
-	// 	switch pathArr[0] {
-	// 	case "user":
-	// 		if id != "" {
-	// 			hdl.UserUpdate(ctx, id)
-	// 		} else {
-	// 			w.Write([]byte("Нет БО"))
-	// 		}
-	// 	default:
-	// 		w.Write([]byte("Нет БО"))
-	// 	}
-	// 	return
-	// }
 }
+
+// эта функция работает плохо
 func sendFile(file string, ctx engine.Context) {
 	pwd, _ := os.Getwd()
-	fmt.Println(pwd)
-	http.ServeFile(ctx.Response, ctx.Request, pwd+"/")
+	fmt.Println(pwd + "/" + file)
+	// проблема в этой команде, она просто не работает с:
+	http.ServeFile(ctx.Response, ctx.Request, file)
 }
 
+// эта функция работает хорошо
 func checkStatic(path string) (string, bool) {
-	fmt.Println(path)
+	// fmt.Println(path)
 	typeFile := strings.Split(path, ".")
 	if len(typeFile) < 2 {
 		return "", false
 	}
-	fmt.Println(typeFile, types[typeFile[1]])
+	// fmt.Println(typeFile, types[typeFile[1]])
 	if _, ok := types[typeFile[1]]; ok {
 		switch typeFile[1] {
 		case "html":
